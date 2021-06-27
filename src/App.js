9d81c43048aca115dada79bf25f3488487d6dda7
vishalkumar13-tech <vishalkumar13july@gmail.com>
2021-06-28 00:17:00 +0530

    Adding changes made in src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+
+class App extends Component {
+  render(){
+
+    return (
+      <div className="App" >
+        {/* <FunctionClick/> */}
+        {/* <ClassClick/> */}
+        <EventBind/>
+          {/* <Counter/> */}
+          {/* <Greet name="Bruce" heroName="Superman" /> */}
+        {/* <Message/> */}
+         {/* <Greet name="Bruce" heroName="Superman" >
+            <p >This is children props</p> 
+         </Greet>
+         <Greet name="clark" heroName="Batman" >
+            <button>Action</button>
+         </Greet>
+         <Greet name="Diana" heroName="Wonder women" />
+         <Welcome name="Bruce" heroName="Superman"/>
+         <Welcome name="clark" heroName="Batman"/>
+         <Welcome name="Diana" heroName="Wonder women"/> */}
+          {/* <Welcome name="Bruce" heroName="Superman"/> */}
+         {/* <Hello /> */}
     </div>
   );
+}
 }
 
 export default App;
